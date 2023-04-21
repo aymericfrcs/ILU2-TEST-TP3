e63bf33ec35d35f07fab0d174d1857d7f409d9f3
aymericfrcs <aymeric.frcs@gmail.com>
2023-04-21 14:15:46 +0200

    EX_1
    EX_1 FINAL

--- src/ilu2/Welcome.java
+++ src/ilu2/Welcome.java
@@ -2,4 +2,13 @@ package ilu2;
 
 public class Welcome {
 
+	public static String welcome(String input) {
+		StringBuilder str = new StringBuilder();
+		
+		str.append("Hello, ");
+		input = input.substring(0, 1).toUpperCase() + input.substring(1);
+		str.append(input);
+		
+		return str.toString();
+	}
 }
---
+++ test/ilu2/WelcomeTest.java
@@ -0,0 +1,21 @@
+package ilu2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WelcomeTest {
+	
+
+	@Test
+	void wlcmTestName() {
+		assertEquals(Welcome.welcome("Bob"), "Hello, Bob");
+		assertEquals(Welcome.welcome("bob"), "Hello, Bob");
+	}
+
+	/*@Test
+	void wlcmTestNullOrEmpty() {
+		assertEquals(Welcome.welcome(null), "Hello, my friend");
+		assertEquals(Welcome.welcome("  "), "Hello, my friend");
+	}*/
+}
